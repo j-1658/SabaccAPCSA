6055c6306261c8f54eb2fe5b0a4a54370706de3c
zimmererJ <105673427+zimmererJ@users.noreply.github.com>
2022-05-16 13:20:23 -0700

    kjh
    jashdkasd

--- src/Game.java
+++ src/Game.java
@@ -7,6 +7,13 @@ public class Game {
     Round currentRound;
     int currentMinBet;
 
+    public Game(int pot, Player[] pList, Deck d, Round cR) {
+        sabaccPot = pot;
+        playerList = pList;
+        deck = d;
+        currentRound = cR;
+        currentMinBet = 10;
+    }
     public void setup(){
 
     }
@@ -28,6 +35,14 @@ public class Game {
     public ArrayList<Card> getDeck(){
         return this.deck.deckList;
     }
+
+    public int getCurrentMinBet() { return this.currentMinBet; }
+
+    public void setCurrentMinBet(int b) { currentMinBet = b; }
+
+    public void setSabaccPot(int p) { sabaccPot = p; }
+
+    public int getSabaccPot() { return this.sabaccPot; }
     public void runGame(){
 
     }
