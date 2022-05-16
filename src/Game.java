b946f568568be089ad5fd1b57feb526c86da18af
idontknow <joshua@macydennis.us>
2022-05-16 12:17:03 -0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,34 @@
-public class Game {
-    public static void main(String[] args){
-        System.out.println("Hi");
-    }
+import java.util.ArrayList;
 
+public class Game {
+    int sabaccPot;
+    Player[] playerList;
+    Deck deck;
+    Round currentRound;
+    int currentMinBet;
+
+    public void setup(){
+
+    }
+    public void sabaccShift(){
+
+    }
+    public void startRound(){
+
+    }
+    public void updateFrame(){
+
+    }
+    public void nextRound(){
+
+    }
+    public Player[] getPlayerList(){
+        return this.playerList;
+    }
+    public ArrayList<Card> getDeck(){
+        return this.deck.deckList;
+    }
+    public void runGame(){
+
+    }
 }
