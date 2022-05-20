9bda1950dad3aef37230233244bf33111f703fee
watanabed0706 <105673471+watanabed0706@users.noreply.github.com>
2022-05-20 12:22:59 -0700

    Merge branch 'main' of https://github.com/j-1658/SabaccAPCSA

--- src/Card.java
+++ src/Card.java
@@ -1,31 +1,31 @@
 public class Card {
-    private boolean inField;
-    private String suit;
-    private int value;
-    public Card(boolean inField, String suit, int value)   {
+    private boolean inField;    //keeps track on whether the card is in the interference field
+    private String suit;    //the string of the suit, for the special cards it is the name of the card itself
+    private int value;  //the int value of the card's worth
+    public Card(boolean inField, String suit, int value)   {    //card constructor
         this.inField = inField;
         this.suit = suit.toUpperCase();
         this.value = value;
     }
-    public void setInField(boolean inField) {
+    public void setInField(boolean inField) {   //used put a card in or out of the interference field
         this.inField = inField;
     }
-    public boolean getInField() {
+    public boolean getInField() {   //used to return weather a card is in the interference field
         return this.inField;
     }
-    public void setSuit(String suit) {
+    public void setSuit(String suit) {  //can be used to change a card's suit
         this.suit = suit.toUpperCase();
     }
     public String getSuit() {
         return this.suit;
-    }
-    public void setValue(int value) {
+    }   //gets the suit
+    public void setValue(int value) {   //can change the int value of a card
         this.value = value;
     }
-    public int getValue()   {
+    public int getValue()   {   //gets the int value of a card
         return this.value;
     }
-    public String stringValue(int v) {
+    public String stringValue(int v) {  //used in the toString method ot return a string version of the int value.
         switch (v)  {
             case 1 : return "ONE";
             case 2 : return "TWO";
@@ -45,7 +45,7 @@ public class Card {
             default: return "";
         }
     }
-    public boolean isSpecial()  {
+    public boolean isSpecial()  {   //checks if a card is 'special'
         if (suit.equals("BALANCE"))
             return true;
         else if (suit.equals("THE IDIOT"))
