f78bd6c4dab0c976697ab1b7519d5110138d526a
idontknow <joshua@macydennis.us>
2022-05-20 13:20:35 -0700

    hi

---
+++ src/DisplayGraphics.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class DisplayGraphics extends Canvas {
+
+    public void paint(Graphics g){
+        //initial scene
+        setBackground(Color.BLACK);
+        g.setColor(Color.getHSBColor(Color.RGBtoHSB()))
+    }
+}
